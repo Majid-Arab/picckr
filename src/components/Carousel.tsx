5883089f145e25718a1cad77f794640afaca6a48
Majid-Arab <arabmajid12@gmail.com>
2025-03-15 12:18:17 +0500

    carousel responsive

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import "swiper/css";
-import "swiper/css/pagination";
-
-// import "./styles.css";
-
-// import required modules
-import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
 
 const slideData = [
   {
@@ -49,18 +44,20 @@ const Carousel = () => {
   return (
     <div className="px-5 sm:px-8 md:px-14 lg:px-16 xl:px-28 py-8 sm:py-14 md:py-20">
       <Swiper
-        slidesPerView={4}
         spaceBetween={30}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        breakpoints={{
+          320: { slidesPerView: 1 },
+          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+          1280: { slidesPerView: 4 },
         }}
         modules={[Autoplay]}
         className="mySwiper"
       >
         {slideData.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div className="flex flex-col justify-center items-center gap-5 w-full h-full flex-shrink-0 ">
+            <div className="flex flex-col justify-center items-center gap-5 w-full h-full flex-shrink-0">
               <div className="p-4 rounded-full border-4 border-yellow-400">
                 <Image
                   src={slide.src}
@@ -71,12 +68,10 @@ const Carousel = () => {
                 />
               </div>
               <div className="text-center">
-                <h3 className="text-xl md:text-2xl text-black  font-semibold text-center">
+                <h3 className="text-xl md:text-2xl text-black font-semibold">
                   {slide.title}
                 </h3>
-                <p className="text-center text-md text-black">
-                  {slide.description}
-                </p>
+                <p className="text-md text-black">{slide.description}</p>
               </div>
             </div>
           </SwiperSlide>
